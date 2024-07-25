b70255d5f4b5f8cbbd17b011e8bf68b057b426e1
jalvarez583 <j_alvarez583@hotmail.com>
2024-07-25 11:40:09 +0200

    first commit

---
+++ app/config/db.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  //url: "mongodb://0.0.0.0:27017/bezkoder_db"
+  url: "mongodb+srv://jorgeacerca:44%3FtegAcu@cluster0.v7vvfkn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/bezkoder_db"
+};
